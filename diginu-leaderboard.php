29faf615052db5d95ec0e1e217c80c708c15b6fb
Belinda Ford <belindav18@gmail.com>
2025-11-02 16:21:31 +0200

    Update URL return format in diginu-leaderboard.php

--- diginu-leaderboard.php
+++ diginu-leaderboard.php
@@ -33,7 +33,7 @@ function diginu_leaderboard_shortcode($atts) {
   ), $atts, 'diginu_leaderboard');
 
   if (empty($atts['src'])) {
-    return '<em>https://docs.google.com/spreadsheets/d/e/2PACX-1vRgL9D3f8unpiKtGBbmQvtqCLg-ZoxSl5epX76sMRaWPng46PcShZHRVx7zxVoeSI2JGiY_-EyPQK12/pub?gid=0&single=true&output=csv</em>';
+    return '<em>"https://docs.google.com/spreadsheets/d/e/2PACX-1vRgL9D3f8unpiKtGBbmQvtqCLg-ZoxSl5epX76sMRaWPng46PcShZHRVx7zxVoeSI2JGiY_-EyPQK12/pub?gid=0&single=true&output=csv"</em>';
   }
 
   // Markup + styles
