5f72138b64d6df37c61db9ba047c352b6cf000bd
Belinda Ford <belindav18@gmail.com>
2025-11-02 16:31:14 +0200

    Update error message for missing 'src' attribute

--- diginu-leaderboard.php
+++ diginu-leaderboard.php
@@ -33,8 +33,9 @@ function diginu_leaderboard_shortcode($atts) {
   ), $atts, 'diginu_leaderboard');
 
   if (empty($atts['src'])) {
-    return '<em>"https://docs.google.com/spreadsheets/d/e/2PACX-1vRgL9D3f8unpiKtGBbmQvtqCLg-ZoxSl5epX76sMRaWPng46PcShZHRVx7zxVoeSI2JGiY_-EyPQK12/pub?gid=0&single=true&output=csv"</em>';
-  }
+  return '<em>Please provide a CSV "src" URL in the shortcode.</em>';
+}
+
 
   // Markup + styles
   $html = '<div class="diginu-lb-wrap">
