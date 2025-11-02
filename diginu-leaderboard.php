3c659db9ab37575ce80c1f63d56a7c1697fe4123
Belinda Ford <belindav18@gmail.com>
2025-11-02 16:26:19 +0200

    Update diginu-leaderboard.php

--- diginu-leaderboard.php
+++ diginu-leaderboard.php
@@ -27,7 +27,7 @@ add_action('wp_enqueue_scripts', 'diginu_leaderboard_assets', 5);
  */
 function diginu_leaderboard_shortcode($atts) {
   $atts = shortcode_atts(array(
-    'src' => '',
+    'src' => 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRgL9D3f8unpiKtGBbmQvtqCLg-ZoxSl5epX76sMRaWPng46PcShZHRVx7zxVoeSI2JGiY_-EyPQK12/pub?gid=0&single=true&output=csv',
     'title' => 'Leaderboard',
     'limit' => '0',
   ), $atts, 'diginu_leaderboard');
